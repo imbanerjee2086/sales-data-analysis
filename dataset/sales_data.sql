eb4906b796b0a381098023dea62d4b79dd91382e
Raj Banerjee <34864796+imbanerjee2086@users.noreply.github.com>
2026-04-24 18:33:07 +0530

    Update sales_data.sql

--- dataset/sales_data.sql
+++ dataset/sales_data.sql
@@ -1,1 +1,15 @@
+CREATE TABLE sales (
+    order_id INT,
+    customer_name VARCHAR(50),
+    product VARCHAR(50),
+    region VARCHAR(50),
+    sales_amount INT,
+    order_date DATE
+);
 
+INSERT INTO sales VALUES
+(1, 'Raj', 'Laptop', 'East', 50000, '2024-01-01'),
+(2, 'Amit', 'Mobile', 'West', 20000, '2024-01-02'),
+(3, 'Sneha', 'Laptop', 'East', 55000, '2024-01-03'),
+(4, 'Raj', 'Tablet', 'North', 15000, '2024-01-04'),
+(5, 'Amit', 'Laptop', 'West', 52000, '2024-01-05');
